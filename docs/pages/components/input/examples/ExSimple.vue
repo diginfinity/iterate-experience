1e93bf53345b2d83a0a55b764a0b26fd9286ce6e
dusan <peric>
2019-05-27 18:19:50 +0200

    added radio buttom component

--- docs/pages/components/input/examples/ExSimple.vue
+++ docs/pages/components/input/examples/ExSimple.vue
@@ -4,6 +4,16 @@
         <et-form> <!-- for testing -->
             <et-page>
                 <et-input v-model="name" name="first_name"></et-input>
+                <div class="block">
+                    <et-radio v-model="name"
+                              native-value="Flint">
+                        Flint
+                    </et-radio>
+                    <et-radio v-model="name"
+                              native-value="Silver">
+                        Silver
+                    </et-radio>
+                </div>
             </et-page>
             <et-page>
                 <et-input v-model="last_name" name="last_name"></et-input>
--- src/components/index.js
+++ src/components/index.js
@@ -3,11 +3,13 @@ import Input from './input'
 import Page from './page'
 import Form from './form'
 import Select from './select'
+import Radio from './Radio'
 
 export {
     Icon,
     Input,
     Page,
     Form,
-    Select
+    Select,
+    Radio
 }
